ee3c6d73f3d7a1e7480f527919ba33654eb88af6
Ishaa Chopra <ishaachopra@v1040-wn-rt-a-60-39.campus-dynamic.uwaterloo.ca>
2023-02-07 10:57:59 -0500

    committed monkey

--- Classes/Net.py
+++ Classes/Net.py
@@ -1,1 +1,43 @@
 import numpy
+from math import exp
+
+class Net:
+    Activation: callable
+    Distance: callable
+    Network: list()#dict
+
+    def __init__(self, dimensions, activation, norm):
+        self.Activation = activation
+        self.Distance = norm
+
+        self._initializeWeights(dimensions)
+
+    def _initializeWeights(self, dimensions, seed=[]):
+        """
+        initialize network (randomly if no seed given)
+        """
+        self.Network = ['STUB']#{'STUB': 0}
+
+    def _evaluate(self, input):
+        """
+        Send input through network and return output (forward prop)
+        """
+        return 0
+
+    def _backProp(self, result, target):
+        """
+        backpropogate errors through network recursively, update weights
+        """
+        self.Network.append('backpropped')
+
+    #Publics
+    def GetLayer(self, depth):
+        """
+        Get layer at given depth
+        """
+        return self.Network[depth]
+
+#Test
+n = Net([], lambda x:1/(1+exp(-1*x)), lambda a,b:abs(a-b))
+n._backProp(3, 3)
+print(n.Network)
